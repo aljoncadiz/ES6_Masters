9cb051c8456073dd7188a8b3f9e7a7f8e0d6c27a
Aljon Cadiz <aljon.cadiz@advantagesolutions.net>
2019-07-31 12:58:50 -0400

    Added Exercise 3 - 6

---
+++ Exercise 3 - 6/Exercise3_6.js
@@ -0,0 +1,40 @@
+var myString = "the quick brown qa jumps over the lazy dev.";
+
+matchResult = ({str, rgx}) => {
+	return str.match(rgx).length;
+};
+
+getRegexByType = ({type}) => {
+	switch(type){
+		case "word": return /\S+/g; break;
+		case "vowel": return /[aeiou]/g; break;
+		case "consonant": return /[b-df-hj-np-tv-z]/g; break;
+	}
+}
+
+printResult = (str, type) => {
+	myString = myString.toLowerCase();
+
+	let strObject = { type: type, str: myString};	//Object Initializer
+	let rgxObject = { type: type, rgx: getRegexByType(strObject)};
+	
+	let mergedObj = { ...strObject, ...rgxObject}; //Spead operator
+
+	let typeCount = matchResult(mergedObj);
+
+	return `${str[0]}${type}${str[1]} ${typeCount}`;
+};
+
+processMyString = (type) => {
+	return printResult`Number of ${type}(s) found:`;
+};
+
+main = (...args) => { //rest operator
+	for(let arg of args){ //For ..of
+		console.log(
+			processMyString(arg)
+		)
+	}
+};
+
+main("word", "vowel", "consonant"); //args for rest operator
--- Exercise1/Exercise1.js
+++ Exercise1/Exercise1.js
@@ -1,27 +1,19 @@
 var myString = "the quick brown qa jumps over the lazy dev.";
 
-matchResult = ({str, rgx}) => {
-	return str.match(rgx).length;
+matchResult = (str, regexStr) => {
+	return str.match(regexStr).length;
 };
-
-getRegexByType = ({type}) => {
-	switch(type){
-		case "word": return /\S+/g; break;
-		case "vowel": return /[aeiou]/g; break;
-		case "consonant": return /[b-df-hj-np-tv-z]/g; break;
-	}
-}
 
 printResult = (str, type) => {
 	myString = myString.toLowerCase();
-
-	let strObject = { type: type, str: myString};	//Object Initializer
-	let rgxObject = { type: type, rgx: getRegexByType(strObject)};
 	
-	let mergedObj = { ...strObject, ...rgxObject}; //Spead operator
-
-	let typeCount = matchResult(mergedObj);
-
+	let typeCount = 0;
+	switch(type){
+		case "word": typeCount = matchResult(myString, /\S+/g); break;
+		case "vowel": typeCount = matchResult(myString, /[aeiou]/g); break;
+		case "consonant": typeCount = matchResult(myString, /[b-df-hj-np-tv-z]/g); break;
+	}
+	
 	return `${str[0]}${type}${str[1]} ${typeCount}`;
 };
 
@@ -29,12 +21,12 @@ processMyString = (type) => {
 	return printResult`Number of ${type}(s) found:`;
 };
 
-main = (...args) => { //rest operator
-	for(let arg of args){ //For ..of
-		console.log(
-			processMyString(arg)
-		)
-	}
+main = () => {
+	return console.log(`
+		${processMyString("word")}
+		${processMyString("vowel")}
+		${processMyString("consonant")}
+	`);
 };
 
-main("word", "vowel", "consonant"); //args for rest operator
+main();
