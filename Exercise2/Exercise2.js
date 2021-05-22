4c2ffe40a5b626a8b369f7eba6472cc4c8ad4090
aljon.cadiz@advantagesolutions.net <aljonrey.cadiz@gmail.com>
2021-05-23 00:37:41 +0800

    Added test

--- Exercise2/Exercise2.js
+++ Exercise2/Exercise2.js
@@ -30,6 +30,7 @@ printResult = (rank, name, score) => {
 
 main = () => {
 	debugger;
+	var test = 1;
 	input.sort(compare);
 	var [top1 ,top2 ,top3] = rankElements(input);
 	!!top1 ? printResult("1st", top1.name, top1.score): null;
